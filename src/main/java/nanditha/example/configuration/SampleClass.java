a1d0f39dff9fe0ebf49e3af0bf5437cfbbfbb9a9
nandithakunduru <kundurunanditha@gmail.com>
2018-04-04 01:52:12 -0500

    This is my sample commit

---
+++ src/main/java/nanditha/example/configuration/SampleClass.java
@@ -0,0 +1,8 @@
+package nanditha.example.configuration;
+
+public class SampleClass {
+
+    public static void main (String args[]){
+        System.out.println("This is the empty file");
+    }
+}
